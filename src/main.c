281d3d4b4385800cc06e91a1509f897c6367d937
Eliott Vallee dore <evallee-@c1r5p7.42quebec.com>
2023-05-26 01:29:00 -0400

    haha funny number:

--- src/main.c
+++ src/main.c
@@ -6,32 +6,46 @@
 /*   By: evallee- <evallee-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 15:53:05 by evallee-          #+#    #+#             */
-/*   Updated: 2023/05/25 20:23:29 by evallee-         ###   ########.fr       */
+/*   Updated: 2023/05/26 01:28:09 by evallee-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "push_swap.h"
 
+static void	populate_list(t_list **list, char **argv)
+{
+	int		*num;
+
+	while (*argv)
+	{
+		num = malloc(sizeof(int *));
+		if (!num)
+		{
+			ft_lstclear(list, free);
+			return ;
+		}
+		*num = ft_atoi(*argv);
+		ft_lstadd_back(list, ft_lstnew(num));
+		argv++;
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	t_list	*a;
 	t_list	*b;
-	int		nums[4];
+	t_list	*temp;
 
 	(void)argc;
-	(void)argv;
+	a = NULL;
 	b = NULL;
-	nums[0] = 10;
-	nums[1] = 240;
-	nums[2] = 2;
-	nums[3] = 1;
-	a = ft_lstnew(&nums[0]);
-	a->next = ft_lstnew(&nums[1]);
-	printf("before : 1: %d 2: %d\n", *((int *)(a->content)), *((int *)(a->next->content)));
-	swap(a);
-	printf("after : 1: %d 2: %d\n", *((int *)(a->content)), *((int *)(a->next->content)));
-	push(&b, &a);
-	printf("b : 1: %d\n", *((int *)(b->content)));
+	populate_list(&a, &argv[1]);
+	temp = a;
+	while (temp)
+	{
+		printf("%d\n", *(int *)(temp->content));
+		temp = temp->next;
+	}
 	return (EXIT_SUCCESS);
 }
